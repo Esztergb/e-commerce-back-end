153bb908e9c8ce11db358e2cd3995510ffdf950c
Eszter Barbuscia <gubiscom@yahoo.com>
2023-07-06 10:08:29 -0700

    set up category and product models

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,30 @@
+// import necessary parts of the sequelize libary
 const { Model, DataTypes } = require('sequelize');
-
+// import database connection
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+//set up colums and rules for category model (given in challange Readme)
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
